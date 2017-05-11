879a0495adf0d94b35ce276f709a955b4ba39bd3
landpack <ada.frank.ak@gmail.com>
2017-05-11 23:43:38 +0800

    my first java

---
+++ hello_word.java
@@ -0,0 +1,15 @@
+//	@Author:	Frank AK
+//	@Date:	2017-5-11
+//	@Desc:	Just for learn java.
+//	@Build:	javac hello_word.java
+//	@Run:	java hello_world
+//	@Git:	git add . | git commit -m 'my first java' | git push origin master
+//	@Git:	git tag -a v0.0.1 | git push origin master tag v0.0.1 
+
+public class HelloWorld
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello Frank AK!");
+	}
+}
