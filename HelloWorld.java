f509fe8b49be2899dc87d941ab420ecbf1feb692
landpack <ada.frank.ak@gmail.com>
2017-05-11 23:38:28 +0800

    my first java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+//	@Author:	Frank AK
+//	@Date:	2017-5-11
+//	@Desc:	Just for learn java.
+//	@Build:	javac hello_word.java
+//	@Run:	java hello_world
+
+public class HelloWorld
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello Frank AK!");
+	}
+}
