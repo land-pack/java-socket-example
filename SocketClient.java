b7116063713457e8ae078c082b0fb9371b0d8d2d
wangxb <wangxb0902@126.com>
2017-05-15 16:02:45 +0800

    fix FrankAK bug

--- SocketClient.java
+++ SocketClient.java
@@ -3,24 +3,37 @@ import java.io.PrintWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketClient{
 	public static void main(String [] args) throws IOException{
-		Socket socket = new Socket("localhost", 9990);
+		Socket socket = new Socket("localhost", 9998);
 		// Only Wait server message coming ~
 		//=================================
-		// InputStreamReader input_data = new InputStreamReader(socket.getInputStream());
-		// BufferedReader input = new BufferedReader(input_data);
-		// String answer = input.readLine();
-		// System.out.println("Received:"+ answer);
-		// Only Send message to the socket server ~
+//		 InputStreamReader input_data = new InputStreamReader(socket.getInputStream());
+//		 BufferedReader input = new BufferedReader(input_data);
+//		 String answer = input.readLine();
+//		 System.out.println("Received:"+ answer);
+//		 Only Send message to the socket server ~
 		//=================================
 		try{
 			PrintWriter output_data = new PrintWriter(socket.getOutputStream(), true);
-			output_data.println("Say hello to socket server~");
+			Scanner input = new Scanner(System.in);
+			String info = null;
+			while (true){
+				System.out.print("Please enter something:");
+				info = input.nextLine();
+				if ("quit".equals(info)){
+					output_data.println("quit");
+					break;
+				}
+				output_data.println(info);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
 		}finally{
 			socket.close();
-		}	
+		}
 	}
 
 }
